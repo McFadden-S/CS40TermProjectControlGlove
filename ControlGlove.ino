5d382d65a3ffdcb9419be998db39642cb5743b98
McFadden-S <shaefromhome@shaw.ca>
2019-03-10 16:41:54 -0500

    V0.2
    -all sensors are now addressed through the array
    -added calibration for sensors to allow for scaled output
    -added method to see calibration to so calibrations can be checked

--- ControlGlove.ino
+++ ControlGlove.ino
@@ -11,53 +11,89 @@
 ADS1015 lsSensor; //sensor for left two fingers, Finger 2 Pinkie, Finger 1 Index
 ADS1015 rsSensor; //sensor for right two fingers. Finger 2 Middle, Finger 1 Pointer
 ADS1015 tSensor; //sensor fo thumb. Finger 1 Thumb 
+ADS1015 Sensors[] = {lsSensor, rsSensor, tSensor};
+int nOfs = 3; //number of sensors
 
 void setup() {
 
   Wire.begin();
   Serial.begin(115200); //when changed for bluetooth set at 38400
-  uint8_t incoming; // variable for data coming through serial
   
-  if(lsSensor.begin(Wire, 100000, ADS1015_ADDRESS_GND) == false){
+  if(Sensors[0].begin(Wire, 100000, ADS1015_ADDRESS_GND) == false){
     Serial.print("!"); //notifies cp error with module
     while(1); //loop to stop program
   }//end of lsSensor start up
   else{
-    Seral.print("#"); //notifies cp know this module is responding
+    Serial.print("#"); //notifies cp know this module is responding
   }//end of else module is working
   
-  if(rsSensor.begin(Wire, 100000, ADS1015_ADDRESS_SDA) == false){
+  if(Sensors[1].begin(Wire, 100000, ADS1015_ADDRESS_SDA) == false){
     Serial.print("!"); //notifies cp error with module
     while(1); //loop to stop program
   }//end of rsSensor start up
   else{
-    Seral.print("#"); //notifies cp know this module is responding
+    Serial.print("#"); //notifies cp know this module is responding
   }//end of else module is working
 
-  if(tSensor.begin(Wire, 100000, ADS1015_ADDRESS_SCL) == false){
+  if(Sensors[2].begin(Wire, 100000, ADS1015_ADDRESS_SCL) == false){
     Serial.print("!"); //notifies cp error with module
     while(1); //loop to stop program
   }//end of tSensor start up
   else{
-    Seral.print("#"); //notifies cp know this module is responding
+    Serial.print("#"); //notifies cp know this module is responding
   }//end of else module is working
 
-  Serial.print("?"); //notifies cp waiting for input to end calibration
-
-  do{
-    lsSensor.calibrate();
-    rsSensor.calibrate();
-    tSensor.calibrate();
-
-    if(Serial.availabe()){
-      incoming = Serial.read();
-    }
-  }while(incoming != '#');
-
+  calibrateSensors();
+  //checkCalibration();
+  
   Serial.print("#"); //notifies cp calibration is complete and program is ready for transmition
 }//end of set up
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
+
+void calibrateSensors(){
+  uint8_t incoming = 0; // variable for data coming through serial
+  
+  for(int i = 0; i < nOfs; i++){
+    Serial.print("?"); //notifies cp waiting for input to end calibration of module 1
+
+    do{
+      Sensors[i].calibrate();
+
+      if(Serial.available()){
+        incoming = Serial.read();
+      }
+    }while(incoming != '#');
+    incoming = 0;
+  }
+}
+
+void checkCalibration(){
+  //Print calibration data
+  Serial.println();
+  for (int s = 0; s < nOfs; s++){
+    Serial.print("Module ");
+    Serial.println(s+1);
+    for (int f = 0; f < 2; f++){
+      Serial.print("Finger ");
+      Serial.print(f+1);
+      Serial.print(": ");
+      for (int hiLo = 0; hiLo < 2; hiLo++){
+        switch (hiLo){
+          case 0:
+            Serial.print("Low: ");
+            Serial.print(Sensors[s].getCalibration(f, hiLo));
+            break;
+          case 1:
+            Serial.print(" High: ");
+            Serial.print(Sensors[s].getCalibration(f, hiLo));
+            break;
+        }//end of switch
+      }//end of high low for loop
+      Serial.println();
+    }//end of channel for loop
+  }//end of sensor for loop
+}//end of check calibration
